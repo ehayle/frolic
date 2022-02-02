5b9c57cf090788b7ddf9ed30a2bcd78244a16763
E <pucadesignstudio@gmail.com>
2022-02-02 11:49:54 -0700

    initial commit

---
+++ mysite/settings/production.py
@@ -0,0 +1,35 @@
+from __future__ import absolute_import, unicode_literals
+from .base import *
+import dj_database_url
+import os
+
+env = os.environ.copy()
+SECRET_KEY = env['SECRET_KEY']
+
+DEBUG = False
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
+#STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+#STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.StaticFilesStorage'
+
+COMPRESS_OFFLINE = True
+COMPRESS_CSS_FILTERS = [
+    'compressor.filters.css_default.CssAbsoluteFilter',
+    'compressor.filters.cssmin.CSSMinFilter',
+]
+COMPRESS_CSS_HASHING_METHOD = 'content'
+
+# Parse database configuration from $DATABASE_URL
+DATABASES['default'] =  dj_database_url.config()
+
+# Honor the 'X-Forwarded-Proto' header for request.is_secure()
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
+# Allow all host headers
+ALLOWED_HOSTS = ['*', 'www.blooming-atoll-76462.herokuapp.com']
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
