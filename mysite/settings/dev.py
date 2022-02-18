8ee42afd91b1501e8dddf998b6fa3a55e01b2c37
E <pucadesignstudio@gmail.com>
2022-02-17 18:52:05 -0700

    testing

--- mysite/settings/dev.py
+++ mysite/settings/dev.py
@@ -1,10 +1,9 @@
 from .base import *
-from decouple import config
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config('SECRET_KEY')
+SECRET_KEY = "k-$-)pl5e%_th&&a^u7tfoz%vtp#w64&ex*-=xhzqv@uuxv1u"
 
 # SECURITY WARNING: define the correct hosts in production!
 ALLOWED_HOSTS = ['*']
