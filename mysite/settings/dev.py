7b06e82fe6f8cfa07c19fd34db4709bb21df5fb7
E <pucadesignstudio@gmail.com>
2022-02-20 15:35:04 -0700

    AWS S3

--- mysite/settings/dev.py
+++ mysite/settings/dev.py
@@ -6,9 +6,6 @@ DEBUG = True
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = config('SECRET_KEY')
 
-AWS_ACCESS_KEY_ID = config('AWS_ACCESS_KEY_ID')
-AWS_SECRET_ACCESS_KEY = config('AWS_SECRET_ACCESS_KEY')
-
 # SECURITY WARNING: define the correct hosts in production!
 ALLOWED_HOSTS = ['*']
 
